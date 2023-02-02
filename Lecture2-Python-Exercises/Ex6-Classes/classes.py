cbfa10c23ea4ef54c6a354654519e4cde65f7cb9
Txell Amigó <txellmgsol@gmail.com>
2023-02-02 21:41:23 +0100

    Classes Exercice, Object Oriented Programming

---
+++ Lecture2-Python-Exercises/Ex6-Classes/classes.py
@@ -0,0 +1,10 @@
+# We create a class
+class Point():
+    def __init__(self, value_x, value_y):
+        self.property_x = value_x
+        self.property_y = value_y
+
+p = Point(2, 8)
+
+print(p.property_x)
+print(p.property_y)
