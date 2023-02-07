18706a6f741e08be6f68bfdaef8b04ff323537e8
Txell Amigó <txellmgsol@gmail.com>
2023-02-07 21:00:12 +0100

    task app, form, client side validation working

--- Lecture3-Django-Exercises/lecture3/tasks/urls.py
+++ Lecture3-Django-Exercises/lecture3/tasks/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "tasks" # We define an app name in order to not get confused about path names: tasks:index, tasks:add
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("add", views.add, name="add"),
 ]
--- Lecture3-Django-Exercises/lecture3/tasks/views.py
+++ Lecture3-Django-Exercises/lecture3/tasks/views.py
@@ -1,9 +1,20 @@
+from django import forms 
 from django.shortcuts import render
 
 tasks = ["resume", "letter", "logo" ]
 # Create your views here.
 
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task") # Name of the new task
+    priority = forms.IntegerField(label = "Priority", min_value=1, max_value=10) 
+     
+
 def index(request):
     return render(request, "tasks/index.html", {
         "tasks": tasks
+    })
+
+def add(request):
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
     })
