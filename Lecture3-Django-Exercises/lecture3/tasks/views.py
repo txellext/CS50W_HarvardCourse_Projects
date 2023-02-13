56347e8c502bcbcbf94fd9e98a81d9ff7d961367
Txell Amigó <txellmgsol@gmail.com>
2023-02-13 10:41:00 +0100

    session included and working

--- Lecture3-Django-Exercises/lecture3/tasks/views.py
+++ Lecture3-Django-Exercises/lecture3/tasks/views.py
@@ -10,10 +10,14 @@ class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task") # Name of the new task
     priority = forms.IntegerField(label = "Priority", min_value=1, max_value=10) 
      
-
+#task = []
 def index(request):
+    if "tasks" not in request.session: # Loocking inside sessions if there is a list of tasks
+        request.session["tasks"] = [] # If not, create an empty list of tasks
+
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        #"tasks": tasks
+        "tasks": request.session["tasks"] # Request the tasks list
     })
 
 def add(request):
@@ -22,7 +26,8 @@ def add(request):
         form = NewTaskForm(request.POST) # All data submitted saved it to form variable
         if form.is_valid(): # Chekc if form is valid
             task = form.cleaned_data["task"] # We take data from form, get the task ans saved it in task variable. 
-            tasks.append(task) # Add task to my growing tasks list
+            #tasks.append(task) # Add task to my growing tasks list
+            request.session["tasks"] += [task] # Append task inside session tasks
             return HttpResponseRedirect(reverse("tasks:index")) # redirect back to index page once task is submited, using reverse function
         else:
             return render(request, "tasks/add.html", { # If not valid render the same data add.html and send back
