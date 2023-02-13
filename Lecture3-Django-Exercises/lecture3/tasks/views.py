298f68578d974c7dcb405b79c274b34f8dbada4a
Txell Amigó <txellmgsol@gmail.com>
2023-02-13 10:10:56 +0100

    server side validation + redirect

--- Lecture3-Django-Exercises/lecture3/tasks/views.py
+++ Lecture3-Django-Exercises/lecture3/tasks/views.py
@@ -1,7 +1,9 @@
 from django import forms 
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.shortcuts import reverse
 
-tasks = ["resume", "letter", "logo" ]
+tasks = []
 # Create your views here.
 
 class NewTaskForm(forms.Form):
@@ -15,6 +17,19 @@ def index(request):
     })
 
 def add(request):
-    return render(request, "tasks/add.html", {
-        "form": NewTaskForm()
+    # Contidional for server validation
+    if request.method == "POST": # check if request method is post, submit form's data
+        form = NewTaskForm(request.POST) # All data submitted saved it to form variable
+        if form.is_valid(): # Chekc if form is valid
+            task = form.cleaned_data["task"] # We take data from form, get the task ans saved it in task variable. 
+            tasks.append(task) # Add task to my growing tasks list
+            return HttpResponseRedirect(reverse("tasks:index")) # redirect back to index page once task is submited, using reverse function
+        else:
+            return render(request, "tasks/add.html", { # If not valid render the same data add.html and send back
+                "form": form # passing it to form, to see errors
+            })
+
+    return render(request, "tasks/add.html", { # Otherwise if user just try to get only the page rather than sumbit data
+        "form": NewTaskForm() # Get an empty form
+        
     })
